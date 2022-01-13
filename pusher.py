545bd1404ca895525d4ae7a1fa62fb0ce592e90e
pinming <zhengtingpeng88@163.com>
2022-01-13 13:57:58 +0800

    v2.0.1 update: 核酸状态自动参照上次填报、改变判断填报成功逻辑

--- pusher.py
+++ pusher.py
@@ -4,12 +4,11 @@ import pytz
 import user_config
 
 ########## 以下部分一般不需要修改，除非你需要自己定制发送邮件或微信推送的内容 ##########
-# 获取公告 / 请勿删除，便于发送需要用户知悉的信息
+# 获取公告 / 请勿删除，便于发送需要用户知悉的信息 
 url_notice = 'https://oss.pm-z.tech/notice_for_autoreport.txt'
 session_notice = requests.session()
 
 url_for_sc = "https://sctapi.ftqq.com/" + user_config.SCKEY + ".send"
-
 
 # ServerChan 推送
 class Pusher(object):
@@ -23,10 +22,14 @@ class Pusher(object):
         # 邮件内容
         content = f'''# 今日健康情况已申报
 ## 📚 申报内容
+
 - 姓名：{site.name}
 - 申报时间：{datetime.datetime.now(pytz.timezone("PRC")).strftime("%Y-%m-%d %H:%M:%S")}
 - 当前位置：{site.szcsmc}
-## ⚡ 注意事项
+- 48 小时核酸检测状态：{site.hsjc_to_string(site.hsjc)}
+
+## ⚠ 注意事项
+
 请确保您的身体状况良好再使用该软件，做到如实申报自身身体状况。
 
 若身体出现异常，应立即停止使用该软件并及时于学校系统更改每日申报情况。
@@ -35,17 +38,20 @@ class Pusher(object):
 
 ## 📢 公告
 本软件仅限于技术用途，切勿滥用！跟进本软件更新，详见 [Github](https://github.com/Pinming/NWPU_COVID19_AutoReport)。\n{notice}'''
-
-        data_for_push = {"title": title, "desp": content, "channel": "9"}
+        data_for_push = {
+            "title": title,
+            "desp": content,
+            "channel": "9"
+        }
         requests.post(url_for_sc, data=data_for_push)
         print('微信推送成功！如果没有收到信息请检查 ServerChan 配置是否有误。')
-
+    
     def sc_push_when_login_failed(site):
         data_for_push_failed = {
             "title":
-            '今日疫情填报失败 | 翱翔门户登陆失败',
+                '今日疫情填报失败 | 翱翔门户登陆失败',
             "desp":
-            '''
+                '''
 ❌ 登入翱翔门户过程失败！
 
 出现该问题有两种可能原因：
@@ -61,18 +67,19 @@ class Pusher(object):
     def sc_push_when_wrong_info(site):
         data_for_wrong_info = {
             "title":
-            '今日疫情填报失败 | 初始化填报信息时出现有误',
+                '今日疫情填报失败 | 初始化或上传填报信息时出现错误',
             "desp":
-            '''
-❌ 初始化填报信息时出现有误！
+                '''
+❌ 初始化或上传填报信息时出现错误！
 
 出现该问题的可能原因：
 - 学校对填报问卷的问题做出了改动；
     该种情况，请持续关注 https://github.com/Pinming/NWPU_COVID19_AutoReport，等待软件更新再试。
-- 本软件获取您上一次填写的地址时出现了错误。
+- 本软件获取您所填写的地理信息时出现了错误。
     该种情况，请联系作者（通过 Github Issue 或邮箱：i@pm-z.tech）并附上信息填报网站「个人中心→我的打卡」页面的截图，便于定位问题！
 
                 '''
         }
         requests.post(url_for_sc, data=data_for_wrong_info)
         print('登陆失败 | 错误提示已通过微信推送。')
+        
--- report.py
+++ report.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 import requests
 import re
 from bs4 import BeautifulSoup
 import location
 import user_config
 from pusher import Pusher
+from lxml import etree
 
 # URL 常量
 url_jrsb = 'http://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp'  # 获取表格并进行操作
@@ -26,8 +29,11 @@ class NWPU_Yqtb_Site(object):
         self.xssjhm = ""
         self.szcsbm = ""
         self.szcsmc = ""
+        self.hsjc = ""
         self.sign = ""
         self.timeStamp = ""
+        self.last_unsubmitted_report_time = ""
+        self.last_submitted_report_time = ""
         self.data_for_submit = None
     
     # 登录
@@ -92,22 +98,27 @@ class NWPU_Yqtb_Site(object):
             'targetUrl':
                 'base64aHR0cDovL3lxdGIubndwdS5lZHUuY24vL3d4L3hnL3l6LW1vYmlsZS9pbmRleC5qc3A=',
         }
+        # 在 `/wx/ry/jrsb.jsp` 页面中，获取 `timeStamp` & `sign`。
         res_jrsb = self.session.post(url_jrsb,
-                                      data=data_for_init,
-                                      headers=header_for_init)
+                                     data=data_for_init,
+                                     headers=header_for_init)
         self.timeStamp = re.findall(re.compile('(?<=&timeStamp=).*(?=\')'),
                                     res_jrsb.text)[0]
         self.sign = re.findall(re.compile('(?<=sign=).*(?=&)'),
                                res_jrsb.text)[0]
+        
+        # 在 `/wx/ry/jrsb.jsp` 页面中，获取 `param_data`。
         param_data_str = re.findall(re.compile('var paramData = (.*?);'),
                                     res_jrsb.text)[2]
+        # 再在 `param_data` 中获取 `name`、`xymc`、`xssjhm` 三个信息。
         self.name = re.findall(re.compile('(?<=userName:\').*(?=\',szcsbm)'),
-                                   param_data_str)[0]
+                               param_data_str)[0]
         self.xymc = re.findall(re.compile('(?<=xymc:\').*(?=\',)'),
                                param_data_str)[0]
         self.xssjhm = re.findall(re.compile('(?<=xssjhm:\').*(?=\')'),
                                  param_data_str)[0]
         
+        # 在 `wx/xg/yz-mobile/rzxx_list.jsp` 中，获取 `szcsmc`，并查 `location.py` 得 `szcsbm`
         rzxx_list_str = self.session.post(url_rzxx_list, data=data_for_init, headers=header_for_init).text
         soup = BeautifulSoup(rzxx_list_str, 'html.parser')
         loc_name = soup.find("span", attrs={"class": "status"}).string
@@ -128,10 +139,28 @@ class NWPU_Yqtb_Site(object):
             if user_config.SC_switcher == 1:
                 Pusher.sc_push_when_wrong_info(self)
             exit()
+        
+        self.hsjc = self.get_last_hsjc_status(data_for_init, header_for_init)
     
     def submit(self):
         # 伪造一次对 Form 页面的请求，获得 JSESSIONID
         self.session.get(url_jrsb)
+        header_for_init = {
+            'User-Agent':
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.26 Safari/537.36',
+            'Host': 'yqtb.nwpu.edu.cn',
+            'cookie': 'JSESSIONID=' + str((self.session.cookies.values()[2])),
+            'accept':
+                'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'upgrade-insecure-requests': '1',
+            'cache-control': 'no-cache'
+        }
+        data_for_init = {
+            'ticket':
+                str((self.session.cookies.values()[1])),
+            'targetUrl':
+                'base64aHR0cDovL3lxdGIubndwdS5lZHUuY24vL3d4L3hnL3l6LW1vYmlsZS9pbmRleC5qc3A=',
+        }
         header_for_submit = {
             "Host": "yqtb.nwpu.edu.cn",
             "Origin": "http://yqtb.nwpu.edu.cn",
@@ -158,36 +187,78 @@ class NWPU_Yqtb_Site(object):
             'xssjhm': self.xssjhm,
             'szcsmc': self.szcsmc,
             'szcsbm': self.szcsbm,
-            'hsjc': '1',  # TODO: 后续再加获取以前记录
+            'hsjc': self.hsjc,
         }
-        self.data_for_submit
-        url_ry_util_with_token = url_ry_util + '?sign=' + self.sign + '&timeStamp=' + self.timeStamp
-        self.session.post(url=url_ry_util_with_token,
-                          data=self.data_for_submit,
-                          headers=header_for_submit)
-        header_for_init = {
-            'User-Agent':
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.26 Safari/537.36',
-            'Host': 'yqtb.nwpu.edu.cn',
-            'cookie': 'JSESSIONID=' + str((self.session.cookies.values()[2])),
-            'accept':
-                'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'upgrade-insecure-requests': '1',
-            'cache-control': 'no-cache'
-        }
-        data_for_init = {
-            'ticket':
-                str((self.session.cookies.values()[1])),
-            'targetUrl':
-                'base64aHR0cDovL3lxdGIubndwdS5lZHUuY24vL3d4L3hnL3l6LW1vYmlsZS9pbmRleC5qc3A=',
-        }
-        res_submit_page_text = self.session.get(url_jrsb,
-                                                data=data_for_init,
-                                                headers=header_for_init).text
-        if (res_submit_page_text.find('重新提交将覆盖上一次的信息')) != -1:
+        
+        # self.judge_last_report_is_today(self.get_last_report_time(data_for_init, header_for_init)) 的意义：
+        # 判断系统当前日期是否与最后一次填报的日期一致。
+        # 如条件为 False，则不一致，认为今日未填报，执行 POST；
+        # 否则跳过 POST 步骤，不再重复填报。
+        if not (self.judge_last_report_is_today(self.get_last_report_time(data_for_init, header_for_init))):
+            url_ry_util_with_token = url_ry_util + '?sign=' + self.sign + '&timeStamp=' + self.timeStamp
+            self.session.post(url=url_ry_util_with_token,
+                              data=self.data_for_submit,
+                              headers=header_for_submit)
+        else:
+            print('今日已填报，无需重复填报！')
+            return
+        
+        # 判断填报成功
+        if (self.judge_last_report_is_today(self.get_last_report_time(data_for_init, header_for_init))):
             print('申报成功！')
             if user_config.SC_switcher == 1:
                 Pusher.scPush(self)
         else:
             print('申报失败，请重试！')
-
+            if user_config.SC_switcher == 1:
+                Pusher.sc_push_when_wrong_info(self)
+    
+    # 获取最近一次日报填写页
+    def get_last_report(self, data, header):
+        # GET 日报填写列表页
+        rzxx_list_page = self.session.get(
+            'http://yqtb.nwpu.edu.cn/wx/xg/yz-mobile/rzxx_list.jsp?type=xs',
+            data=data,
+            headers=header)
+        # 获取最近一次日报填写页的 URL
+        url_last_detail = "http://yqtb.nwpu.edu.cn" + \
+                          etree.HTML(rzxx_list_page.text).xpath('//*[@id="form1"]/div/a[1]/@href')[0]
+        # 获取最近一次日报填写页
+        detail_page = self.session.get(url_last_detail, data=data, headers=header)
+        return detail_page.text
+    
+    # 获取上一次核酸检测的状态
+    def get_last_hsjc_status(self, data, header):
+        # 获取最近一次日报核酸检测状态：'已检测' or '未检测'
+        nuc_acid_test_status = \
+            etree.HTML(self.get_last_report(data, header)).xpath(
+                '/html/body/div[1]/div[2]/div/div[2]/div[2]/div[2]/text()')[0]
+        if nuc_acid_test_status == '已检测':
+            return '1'
+        else:
+            return '0'
+    
+    def hsjc_to_string(self, hsjc_status):
+        if (hsjc_status == '1'):
+            return '已检测'
+        else:
+            return '未检测'
+    
+    # 把字符串转成 datetime
+    def string_toDatetime(self, st):
+        return datetime.strptime(st, "%Y-%m-%d %H:%M:%S")
+    
+    # 判断上一次填报是否为今天
+    def judge_last_report_is_today(self, report_time):
+        report_time = self.string_toDatetime(report_time)
+        if report_time.date() == datetime.today().date():
+            return True
+        else:
+            return False
+    
+    # 获取最近一次填报的时间，判断是否为今天
+    def get_last_report_time(self, data, header):
+        last_report_time = \
+            etree.HTML(self.get_last_report(data, header)).xpath(
+                '/html/body/div[1]/div[2]/div/div[2]/div[1]/div[2]/text()')[0]
+        return last_report_time
